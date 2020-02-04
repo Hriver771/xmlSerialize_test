edd2808ff900941bac6178cd502ce090247f1fd2
haminji <h.river771@gmail.com>
2020-02-04 16:16:53 +0900

    object xml serialize and deserialize

---
+++ DataManage_v1/MainWindow.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Xml.Serialization;
+
+
+namespace DataManage_v1
+{
+    /// <summary>
+    /// MainWindow.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        ObservableCollection<Object> listData;
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            listData = new ObservableCollection<Object>();
+            list.ItemsSource = listData;
+
+            //var obj = new Object();
+            //var writer = new System.Xml.Serialization.XmlSerializer(typeof(Object));
+            //var wfile = new System.IO.StreamWriter(@"C:/Users/Stradvision/source/repos/DataManage_v1/DataManage_v1/test.xml");
+            //writer.Serialize(wfile, obj);
+            //wfile.Close();
+
+            //// Now we can read the serialized book ...  
+            ////System.Xml.Serialization.XmlSerializer reader =
+            ////    new System.Xml.Serialization.XmlSerializer(typeof(Object));
+            ////System.IO.StreamReader file = new System.IO.StreamReader(
+            ////    @"C:/Users/Stradvision/source/repos/DataManage_v1/DataManage_v1/test.xml");
+            ////Object overview = (Object)reader.Deserialize(file);
+            ////file.Close();
+
+            ////Console.WriteLine(overview.Type);
+
+
+            XmlSerializer deserializer = new XmlSerializer(typeof(Object));
+            TextReader reader = new StreamReader(@"C:/Users/Stradvision/source/repos/DataManage_v1/DataManage_v1/test.xml");
+
+            //object obj = deserializer.Deserialize(reader);
+            Object overview = (Object)deserializer.Deserialize(reader);
+            listData.Add((Object)overview);
+            reader.Close();
+
+        }
+    }
+
+
+}
---
+++ DataManage_v1/Object.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace DataManage_v1
+{
+    public class Object
+    {
+        [XmlElement("ShapeType")]
+        public int ShapeType { get; set; }
+
+        [XmlElement("Type")]
+        public int Type { get; set; }
+
+        [XmlElement("Transform")]
+        public _Transform Transform { get; set; }
+
+        [XmlElement("Position")]
+        public _Position Position { get; set; }
+
+        [XmlElement("Points")]
+        public List<_Point> Points { get; set; }
+
+        [XmlElement("Label")]
+        public _Label Label { get; set; }
+
+        public Object()
+        {
+            this.ShapeType = 0; this.Type = 1;
+            this.Transform = new _Transform();
+            this.Position = new _Position();
+            this.Points = new List<_Point>();
+            _Point temp = new _Point(256, 694);
+            this.Points.Add(temp);
+            _Point temp2 = new _Point(359, 734);
+            this.Points.Add(temp);
+            this.Label = new _Label();
+                      
+        }
+
+        public class _Transform
+        {
+            public double Height { get; set; }
+            public double Width { get; set; }
+            public int ShowRoof { get; set; }
+            public int ExtendedPoint { get; set; }
+            public int Side { get; set; }
+            public int Direction { get; set; }
+
+            public _Transform()
+            {
+                this.Height = 103; this.Width = 40; this.ShowRoof = 0; this.ExtendedPoint = 0; this.Side = -1;
+            }
+        }
+        public class _Position
+        {
+            public double Bottom { get; set; }
+            public double Right { get; set; }
+            public double Top { get; set; }
+            public double Left { get; set; }
+
+            public _Position ()
+            {
+                this.Bottom = 359; this.Right = 734; this.Right = 734; this.Left = 694;
+            }
+        }
+        public class _Points
+        {
+            public _Point point1 { get; set; }
+            public _Point Point2 { get; set; }
+        }
+        public class _Point
+        {
+            public double y { get; set; }
+            public double x { get; set; }
+
+            public _Point() { }
+
+            public _Point(int y, int x)
+            {
+                this.y = y;
+                this.x = x;
+            }
+        }
+        public class _Label
+        {
+            public string Text { get; set; }
+        }
+    }
+}
